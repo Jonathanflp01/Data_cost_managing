6936cac5d4b582cffea75698f4dee54337df0a8d
Jonathanflp01 <jonatanflp01@gmail.com>
2025-11-27 20:54:23 +0100

    Nuevo proyecto

---
+++ modulos/almacenamiento.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from pathlib import Path
+# import os
+
+# forma con libreria OS, método anterior
+# materials_data = os.path.join('..', 'data', 'materiales_db.csv')
+# materials_df = pd.read_csv(materials_data)
+
+
+def cargar_materiales():
+    ruta_actual = Path(__file__).resolve().parent
+    ruta_data = ruta_actual.parent / 'data'
+    ruta_csv = ruta_data / 'materiales_db.csv'
+    try:
+        materiales_df = pd.read_csv(ruta_csv)
+        return materiales_df
+    except FileNotFoundError:
+        print(f"Error: El archivo {ruta_csv} no se encontró.")
+        return pd.DataFrame()
+    except pd.errors.EmptyDataError:
+        print(f"Error: El archivo {ruta_csv} está vacío.")
+        return pd.DataFrame()
+
+
+print("")
+# Prueba de la función
+print(cargar_materiales())
