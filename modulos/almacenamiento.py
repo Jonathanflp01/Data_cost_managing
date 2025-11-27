4aa4be3cf7843f042fec178831d9926999cff7f0
Jonathanflp01 <jonatanflp01@gmail.com>
2025-11-28 00:55:45 +0100

    Actualización_avance_D2

--- modulos/almacenamiento.py
+++ modulos/almacenamiento.py
@@ -1,10 +1,21 @@
 import pandas as pd
 from pathlib import Path
 # import os
+import time
 
 # forma con libreria OS, método anterior
 # materials_data = os.path.join('..', 'data', 'materiales_db.csv')
 # materials_df = pd.read_csv(materials_data)
+inicio = time.time()
+
+
+def estructurar_lista(lista):
+    try:
+        lista_est = {item["material"]: {
+            "precio": item["precio"], "unidad": item["unidad"]} for item in lista}
+        return lista_est
+    except:
+        return {}
 
 
 def cargar_materiales():
@@ -12,8 +23,10 @@ def cargar_materiales():
     ruta_data = ruta_actual.parent / 'data'
     ruta_csv = ruta_data / 'materiales_db.csv'
     try:
-        materiales_df = pd.read_csv(ruta_csv)
-        return materiales_df
+        materiales_df = pd.read_csv(ruta_csv, sep=';')
+        materiales_dic = materiales_df.to_dict(orient="records")
+
+        return materiales_dic
     except FileNotFoundError:
         print(f"Error: El archivo {ruta_csv} no se encontró.")
         return pd.DataFrame()
@@ -26,5 +39,21 @@ def agregar_materiales():
     return
 
 
+def cargar_proyecto(nombre):
+    return
+
+
+def guardar_proyecto(nombre, datos):
+    return
+
+
 # Prueba de la función
-print(cargar_materiales())
+
+lista = cargar_materiales()
+
+print(estructurar_lista(lista))
+
+
+fin = time.time()
+tiempo_ejecucion = fin - inicio
+print(f"El tiempo de ejecución fue de: {tiempo_ejecucion} segundos")
